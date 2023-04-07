bc67e6eb873cc9eeab77a6187d887538d550a87c
chrispysz <krzysztof.pysz99@gmail.com>
2023-04-07 19:15:44 +0200

    port for heroku

--- init.go
+++ init.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+	"fmt"
 
 	_ "github.com/microsoft/go-mssqldb"
 )
@@ -30,11 +32,21 @@ func enableCors(w *http.ResponseWriter) {
 	}
 
 func main() {
-	
+
 
 	//initializeDatabase()
 
 	http.HandleFunc("/predict", predictionHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(GetPort(), nil))
 
+}
+
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "8080"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
 }
